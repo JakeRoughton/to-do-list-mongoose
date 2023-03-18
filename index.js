5a86d1a507fe5d737288898f96f73e54a55e6074
Jake Roughton <112968037+JakeRoughton@users.noreply.github.com>
2023-03-18 17:08:23 -0400

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const indexRoutes = express.Router();
-
-/* GET home page. */
-indexRoutes.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
-
-module.exports = indexRoutes;
